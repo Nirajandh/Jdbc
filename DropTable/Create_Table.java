437a175eeef3ea88900240f433dc8ae88bdc2a5e
Nirajandh <151948730+Nirajandh@users.noreply.github.com>
2023-11-30 11:01:07 +0530

    Add files via upload

---
+++ DropTable/Create_Table.java
@@ -0,0 +1,20 @@
+package jdbcdemo;
+import java.sql.*;
+import java.util.*;
+public class Create_Table {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+        // Scanner sc=new Scanner(System.in);
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","root");
+		Statement st=con.createStatement();
+		//int i=st.executeUpdate("create table emps (id int , name varchar(20))");
+	
+		String sqlquery=" drop table niru";
+		st.executeUpdate(sqlquery);
+		System.out.println("table droped");
+	    con.close();	}
+	
+	
+	}
+
